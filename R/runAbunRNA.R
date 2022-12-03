01235db2b5e8f4b510de98edb771ad1ef4eaf9d3
ChengYue Zhang <shengyue.zhang@mail.utoronto.ca>
2022-12-03 14:25:19 -0500

    runAbunRNA.R upload

---
+++ R/runAbunRNA.R
@@ -0,0 +1,28 @@
+#' Launch Shiny App for AbunRNA
+#'
+#' A function that launches the Shiny app for AbunRNA。
+#' The shiny app permits to generate count matrix, plot heatmap and
+#' generate PCA plot.
+#'
+#'
+#' @return No return value but open up a Shiny page.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' AbunRNA::runAbunRNA()
+#' }
+#'
+#' @references
+#' Grolemund, G. (2015). Learn Shiny - Video Tutorials. \href{https://shiny.rstudio.com/tutorial/}{Link}
+#'
+#' @export
+#' @importFrom shiny runApp
+
+runIntegrationScore <- function() {
+    appDir <- system.file("shiny-scripts",
+                          package = "AbunRNA")
+    shiny::runApp(appDir, display.mode = "normal")
+    return()
+}
+# [END]
